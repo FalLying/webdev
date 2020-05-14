e3bd72930ec386c88631509e31e158af26a6c3ae
Jonas Brito <jonasexplore@gmail.com>
2020-05-14 15:35:13 -0300

    Finish advanced js

---
+++ ES10/script.js
@@ -0,0 +1,18 @@
+// .flat()
+
+const array = [1,2,3,4,5];
+array.flat()
+
+const array2 = ['bob', 'july',,,,,,'john'];
+array2.flat();
+
+// .flatMap()
+
+// its basically, the junction function Map and Flat.
+
+// trimStart() and trimEnd()
+
+// .fromEntries()
+// Transforma uma array em um objeto.
+// .Entries()
+// Faz basicamente o oposto, transforma um objeto em array. 
