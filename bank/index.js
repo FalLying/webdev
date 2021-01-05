8a96261ee5a7ee67091afe22a043955012d35eed
Jonas Brito <jonasexplore@gmail.com>
2021-01-05 13:19:53 -0300

    classe abstrata, polimorfismo e encapsulamento.

--- bank/index.js
+++ bank/index.js
@@ -1,23 +1,13 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", "11122233312");
+const diretor = new Diretor("Rodrigo", 10000, 12345678910);
+const gerente = new Gerente("Maria", 5000, 21346579801);
 
-const cliente2 = new Cliente("Maria", "44455566634");
+diretor.cadastrarSenha("123");
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
-const contaCorrenteMaria = new ContaCorrente(1002, cliente2);
+const estaLogado = SistemaAutenticacao.login(diretor, "123");
 
-contaCorrenteRicardo.depositar(1000);
-const sacou = contaCorrenteRicardo.sacar(342);
-
-const transferiu = contaCorrenteRicardo.transferir(431, contaCorrenteMaria);
-
-console.log(`Você sacou R$${sacou.toFixed(2)} da sua conta.`);
-console.log(
-  `Você transferiu R$${transferiu.toFixed(2)} da sua para ${
-    contaCorrenteMaria.cliente.nome
-  }.`
-);
-console.log("Número de contas: ", ContaCorrente.numeroDeContas);
-console.log(contaCorrenteRicardo);
+console.log(estaLogado);
